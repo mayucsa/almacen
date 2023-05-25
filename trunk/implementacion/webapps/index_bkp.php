2ee22e475eba645e4b1897ad77c0a54ee3934e3f
Ismael López Castrejón <izlopcast2017@gmail.com>
2023-05-25 08:31:50 -0600

    update
    sin cambios

--- trunk/implementacion/webapps/index_bkp.php
+++ trunk/implementacion/webapps/index_bkp.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="es">
 <head>
@@ -20,47 +21,60 @@
 	<link rel="icon" href="includes/imagenes/favicon.png">
 </head>
 
+<div class="modal fade" id="myLoading" tabindex="-3" data-backdrop="static" data-keyboard="false" style="padding-top:20%; overflow-y:visible;" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+	<div align="center"><img src="includes/imagenes/loading3.gif" width="140px"></div>
+	<div id="divtextloading" align="center" style="font-weight:bold; font-size:20px; color:#FFFFFF">Espere un momento...</div>
+</div>
+
+<div class="modal fade" id="modalMensajes" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="padding-top:10%; overflow-y:visible;" >
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header modal-danger">
+				<h5 class="modal-title" id="encabezadoModal"></h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body" id="cuerpoModal"></div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Aceptar</button>
+			</div>
+		</div>
+	</div>
+</div>
+
 <body>
-	<div class="container-fluid">
-		<div class="row card">
-			<div class="col-md-4 offset-md-4">
-				<form class="box p-4 text-center" onsubmit="return false" method="post" autocomplete="off">
-					<h1>Almacen</h1>
-					<p class="text-muted"> Acceda con su número de empleado</p>
-					<input type="text" class="form-control" name="p_usuario" id="p_usuario" placeholder="Número de empleado">
-					<input type="password" class="form-control" name="p_password" id="p_password" placeholder="Contraseña">
-					<!-- <a class="forgot text-muted" href="#">¿Has olvidado su contraseña?</a> -->
-					<button class="btn btn-dark" onclick="iniciarSesion()">
-						Acceso
-					</button>
-					<!-- <button class="btn btn-primary" onclick="iniciarSesion('p_usuario', 'p_password')"> Acceso</button> -->
-					<img class="img-fluid" src="includes/imagenes/Mayucsap.png">
-				</form>
-			</div>
-		</div>
-	</div>
-
-	<div class="modal fade" id="myLoading" tabindex="-3" data-backdrop="static" data-keyboard="false" style="padding-top:20%; overflow-y:visible;" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-		<div align="center"><img src="includes/imagenes/loading3.gif" width="140px"></div>
-		<div id="divtextloading" align="center" style="font-weight:bold; font-size:20px; color:#FFFFFF">Espere un momento...</div>
-	</div>
-
-	<div class="modal fade" id="modalMensajes" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" style="padding-top:10%; overflow-y:visible;" >
-		<div class="modal-dialog" role="document">
-			<div class="modal-content">
-				<div class="modal-header modal-danger">
-					<h5 class="modal-title" id="encabezadoModal"></h5>
-					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-						<span aria-hidden="true">&times;</span>
-					</button>
-				</div>
-				<div class="modal-body" id="cuerpoModal"></div>
-				<div class="modal-footer">
-					<button type="button" class="btn btn-secondary" data-dismiss="modal">Aceptar</button>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="card">
+					<form class="box" onsubmit="return false" method="post" autocomplete="off">
+						<h1>Almac&eacute;n</h1>
+						<p class="text-muted"> Acceda con su número de empleado</p>
+							<input type="text" name="p_usuario" id="p_usuario" placeholder="Número de empleado">
+							<input type="password" name="p_password" id="p_password" placeholder="Contraseña">
+							<!-- <a class="forgot text-muted" href="#">¿Has olvidado su contraseña?</a> -->
+							<input type="submit" name="" value="Acceso" href="#" onclick="iniciarSesion()">
+							<!-- <button class="btn btn-primary" onclick="iniciarSesion('p_usuario', 'p_password')"> Acceso</button> -->
+							<img class="img-fluid" src="includes/imagenes/Mayucsap.png">
+					</form>
 				</div>
 			</div>
 		</div>
 	</div>
-
 </body>
 </html>
+
+<!--Start of Tawk.to Script-->
+<!-- <script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61ef09ab9bd1f31184d91df9/1fq6rrhl1';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script> -->
+<!--End of Tawk.to Script-->
